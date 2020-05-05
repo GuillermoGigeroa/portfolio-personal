3c72d778552de46d8fd9edbcae40375790b1e47e
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2020-05-05 13:00:24 -0300

    Se agrega lo esencial para el trabajo práctico

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,27 @@
 function accionBoton()
 {
-    alert("Hola");
+    var textoNombre = document.getElementById("nombre").value;
+    var textoMail = document.getElementById("mail").value;
+    var textoMensaje = document.getElementById("mensaje").value;
+    if (textoNombre == "")
+    {
+        alert("No se ha ingresado ningún nombre.");
+        return;
+    }
+    if (textoMail == "")
+    {
+        alert("No se ha ingresado ningún mail.");
+        return;
+    }
+    if (textoMensaje == "")
+    {
+        alert("No se ha escrito ningún mensaje");
+        return;
+    }
+    var textoCompleto = textoNombre+'\n'+textoMail+'\n'+textoMensaje;
+    var textoCompleto = "Se ha ingresado correctamente:"+'\n';
+    textoCompleto += "Nombre: "+textoNombre+'\n';
+    textoCompleto += "Mail: "+textoMail+'\n';
+    textoCompleto += "Mensaje: "+textoMensaje;
+    alert(textoCompleto);
 }
