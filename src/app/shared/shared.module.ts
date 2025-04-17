767f109f46e9690bd68aae3e81c5dc9e03ca8971
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2025-04-17 15:09:17 -0300

    Se aplican mejoras estéticas completas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { SharedRoutingModule } from './shared-routing.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        FormsModule,
         SharedRoutingModule,
     ],
     declarations: [
