1f5861aaa46623fb5e7e653518a8ad558f33ce3d
Guillermo Gigeroa <guillermo.gigeroa@hotmail.com>
2025-10-02 13:42:34 -0300

    Se revisa código

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+export class Data {
+  constructor(public name: string, public value: boolean) {}
+}
+
 @Component({
     selector: 'app-skills',
     templateUrl: './skills.component.html',
@@ -8,8 +12,29 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
   showMenu = false;
+  skills: Data[] = [
+    new Data('Formación académica', false),
+    new Data('Lenguajes y conocimientos técnicos', false),
+    new Data('Software', false),
+    new Data('Paquete Microsoft Office', false),
+    new Data('Sistemas Operativos', false),
+    new Data('Idiomas', false),
+    new Data('Más conocimientos', false),
+  ];
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
+  }
+
+  skill(name: string) {
+    this.toggle(name, true);
+  }
+
+  private toggle(name: string, status?: boolean) {
+    this.skills.forEach(element => {
+      if (element.name === name) {
+        element.value = (status !== undefined ? status : !element.value);
+      }
+    });
   }
 }
